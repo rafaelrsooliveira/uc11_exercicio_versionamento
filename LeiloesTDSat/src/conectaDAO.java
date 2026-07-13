89c236fa7a5c23af527b69a1a4efefaf549d3311
Rafael S. Oliveira <rafael.rso@live.com>
2026-07-12 21:40:13 -0300

    Bug no getConnection: corrigida a string jdbc.

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -16,7 +16,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=root&useSSL=false");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
